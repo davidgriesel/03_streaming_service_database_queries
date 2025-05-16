a1b0d5829f06000c50dab08e45a4183a9f7a69b1
davidgriesel <161476008+davidgriesel@users.noreply.github.com>
2025-05-16 17:50:04 +0200

    Update

--- sql-queries/01-profiling.sql
+++ sql-queries/01-profiling.sql
@@ -32,7 +32,7 @@ ORDER BY table_type;
 -- Count the number of records in each table to gain an understanding of relative
 -- table sizes, and identify any unexpectedly empty or undersized tables.
 
--- TABLES
+-- BASE Tables
 SELECT
     'actor' AS table_name,
     COUNT(*) AS row_count
@@ -168,17 +168,12 @@ FROM
 
 -- INSIGHTS
 -- No empty tables found.
-
 -- The largest tables (rental and payment) are clearly transactional.
-
 -- Join tables (film_actor, film_category) and inventory are relatively large.
-
--- Dimension tables (e.g. customer, film, address) of appropriate mid-size.
-
--- Very small tables (e.g. language, staff, store) consistent with lookup role.
-
--- Five of the seven view tables appear to represent enriched entity listings
--- while the remaining two suggest summarised sales data by category and by store.
+-- Dimension tables (e.g. customer, film, address) are of appropriate mid-size.
+-- Very small tables (e.g. language, staff, store) are consistent with lookup role.
+-- Five view tables appear to represent enriched entity listings.
+-- Two view tables suggest summarised sales data by category and by store.
 
 -- RECOMMENDATIONS
 -- Flag very small tables like staff and store for possible manual inspection.
@@ -192,8 +187,7 @@ FROM
 -- data formats, and possible irregularities (e.g., NULLs, strange encodings,
 -- unexpected field usage) before deeper integrity and quality checks.
 
--- PUBLIC TABLES
-
+-- BASE Tables
 SELECT * FROM actor LIMIT 5;
 SELECT * FROM address LIMIT 5;
 SELECT * FROM category LIMIT 5;
@@ -210,8 +204,7 @@ SELECT * FROM rental LIMIT 5;
 SELECT * FROM staff LIMIT 5;
 SELECT * FROM store LIMIT 5;
 
--- VIEW TABLES
-
+-- VIEWs
 SELECT * FROM actor_info LIMIT 5;
 SELECT * FROM customer_list LIMIT 5;
 SELECT * FROM film_list LIMIT 5;
@@ -222,56 +215,41 @@ SELECT * FROM sales_by_store LIMIT 5;
 
 -- INSIGHTS
 -- No structural anomalies were detected.
-
--- Visible NULLs (address2), and empty strings (phone, postal_code) in address table
--- suggesting optional or inconsistently captured contact data.
-
+-- Visible NULLs (address2), and empty strings (phone, postal_code) in address
+-- table suggesting optional or inconsistently captured contact data.
 -- The customer table contains two fields (active and activebool) that seemingly
 -- have a similar function.
-
 -- The film table contain dense columns with non-standard data types (description
 -- type text, fulltext type tsvector) and constrained categorical information
 -- (rating type ENUM).
-
 -- The film_actor and film_category follow expected join-table structure with
 -- composite keys.
-
--- The inventory table records the physical stock of film copies held at each
--- store, linking inventory_id to both film and store.
+-- The inventory table records the physical stock of film copies held at each store,
+-- linking inventory_id to both film and store.
 -- The payment and rental tables contain transaction records linked to other tables
 -- via foreign keys.
-
--- The staff table contains two staff records with visible nulls in specialised 
+-- The staff table contains two staff records with visible nulls in specialised
 -- fields (picture type bytea) and duplicates (password).
-
 -- The last_update fields across all tables seeminlgy share the same timestamp.
 -- Some inconsistencies (active vs activebool) and ambiguity (cagegory.name vs.
 -- city.city)in column names.
-
 -- View tables provide entity listings with foreign keys substituted with
 -- descriptive fields, and aggregated sales data per category and store.
 
 -- RECOMMENDATIONS
--- Remove columns with NULLs, optional information, duplicate functions, dense variables,
--- specialised fields, fields containing duplicates, and any other columns not
--- needed in analysis for removal (Refer 4.3).
-
+-- Remove columns with NULLs, optional information, duplicate functions, dense
+-- variables, specialised fields, fields containing duplicates, and any other
+-- columns not needed in analysis for removal (Refer 4.3).
 -- Impute columns with empty strings with placeholder values like 'Unknown'
 -- (Refer 4.7).
-
 -- Confirm ENUM values (Refer 2.2.7).
-
 -- Confirm composite key integrity in join tables (Refer 2.2.1).
-
 -- Confirm if inventory table contains duplicate copies of film titles per store
 -- distinguished only by inventory_id. (Refer 3.3).
-
--- Communicate shared passwords to management as potential security issue (Reporting).
-
+-- Communicate shared passwords to management as potential security issue
+-- (Reporting).
 -- Confirm if the database has a static last_update date (Refer 3.7.1).
-
 -- Rename columns with inconsistent naming or ambiguous names for standardisation
--- Refer 4.4).
-
+-- (Refer 4.4).
 -- Drop and recreate VIEWs to ensure accuracy and alignment with the schema
 -- (Refer 4.1).
